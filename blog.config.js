436975c8d776a3f26cff5d0d9e4ec18dd77e0ce1
Jun He <mr.automan@qq.com>
2019-08-05 01:21:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,26 +1,26 @@
 module.exports =  {
   // 首页 logo 展示数据
-  github: 'https://github.com/loo41',
-  email: 'lovelife@tcyong.com.cn',
-  qq: '1805170243',
+  github: 'https://github.com/',
+  email: 'aa@gmail.com',
+  qq: '2345678',
   // 输入框默认值
-  title: 'TCYong | Blog',
+  title: 'June | Blog',
   // 是否其他作者发布的文章是否展示, 更具 user.name 做对比
   author: true,
   // 背景图数组，随机展示
   bg_img: [
-    require('./src/assets/index_bg.jpg'),
     require('./src/assets/index_bg2.jpg'),
-    require('./src/assets/index_bg3.jpg'),
+    require('./src/assets/index_bg.jpg'),
     require('./src/assets/index_bg4.jpg'),
+    require('./src/assets/index_bg3.jpg'),
     require('./src/assets/index_bg5.png'),
     require('./src/assets/index_bg6.jpg')
   ],
   // 登陆 github 相关信息
   user: {
-    name: "loo41",
+    name: "imhejun",
     repo: "Blog",
-    client_id: '248302c725b74ef60d01',
-    client_secret: 'ae5338311e2f36cd2b45fc0aa2e8cac9e9cc707f'
+    client_id: 'd0f2632f522fcaf07e67',
+    client_secret: 'a559b074d090407ef8c6a6b25f0b690c4ad5374e'
   },
 }
